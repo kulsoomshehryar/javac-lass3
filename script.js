6445395e092343ef0d8d89d4c3965e93e6d940e7
kulsoomshehryar <kulsoomkulsoomshehryar@gmail.com>
2025-02-10 15:57:33 +0500

    class3

---
+++ script.js
@@ -0,0 +1,49 @@
+// prompt
+// let user1 = prompt("Whats Your Name");
+// alert("hello" +" "+ user1);
+// console.log( typeof user);
+// let user = +prompt("Enter Any Number You Want");
+// console.log(user + "10");
+// let userNum = prompt("Animal" , defaultnumber);
+// document.write(userNum)
+// console.log((parseInt("122.890")));
+// console.log((parseFloat("12344.446")));
+// console.log(Number("8978.878"));
+// // chap:if statement
+// if(true){
+//     console.log("running");
+// }
+// if(false){
+//     console.log("run")
+// }
+// console.log(6=="6"); it is true that its datatype is same
+// console.log(6=="10");check only datatype that it is same
+// console.log(6==="10");check  everything datatype and num or string
+// let secretNum= 5;
+// let userguess = Number(prompt("guess a number"));
+// if(secretNum == userguess){
+//     console.log("In Your Acoount 1000$ Are Including");
+// }
+// else(user > secretNum){
+//     console.log("Your guessed Number Is Greater Than Our secret Number");
+
+// }
+// if(user < secretNum){
+//     console.log("Your guessed Number Is Less Than Our secret Number");
+// }
+// else{
+//     console.log("Write It Properly");
+// }
+// important
+// falsy value "" , 0 ,-0 ,null ,undefined , NaN ,false
+   let i = prompt("Enter a number:");
+
+if (i % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+
+
+    
+
